d100b54fd367c4351419d4400bf62951eb2c67f6
Cian <creepey.jr@gmail.com>
2026-03-10 18:02:14 +0000

    Working through ex2

---
+++ lab4/ex2.js
@@ -0,0 +1,39 @@
+const students = [
+    { name: "Alice", grade: 72 },
+    { name: "Bob",   grade: 45 },
+    { name: "Carol", grade: 88 },
+    { name: "Dan",   grade: 55 },
+    { name: "Eve",   grade: 91 },
+];
+
+// custom function
+function passCheck(student) {
+    /*
+    console.log(student);
+    console.log(student["name"]);
+    console.log(student["grade"]);
+    */
+    if (student["grade"] >= 60) {
+        return student
+    }
+}
+
+function gradeStrip(student) {
+    return student["name"];
+}
+
+// loop through array, annouce every student's name and grade
+for (let i = 0; i < students.length; i++) {
+    console.log(students[i]["name"],":", students[i]["grade"]);
+}
+
+
+// Call function
+let passes = students.filter(passCheck);
+//console.log("passes :", passes);
+
+let nameOnly = passes.map(gradeStrip);
+console.log("Passing students :",nameOnly.length);
+console.log("Passing names :", nameOnly);
+
+const totalGrade = students.reduce()
